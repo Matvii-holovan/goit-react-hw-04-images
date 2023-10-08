f392f65ce0323cdd0291bfde501d95588b231d7c
matvii-holovan <golovanmatvij@gmail.com>
2023-10-08 15:26:19 +0200

    task

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Button = ({ handleMoreImage }) => (
+  <button onClick={handleMoreImage} type="button" className="Button">
+    Load more
+  </button>
+);
+
+export default Button;
